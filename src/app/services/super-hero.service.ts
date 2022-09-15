0976092128f39d03f32cd11b5a9c4a9d7a163236
Jose Victor Persset <jpersset@gmail.com>
2022-09-15 16:51:36 -0300

    Rmv: Remove app.component.html default info and initial implementations to super-hero-service

---
+++ src/app/services/super-hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { SuperHero } from '../models/super-hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SuperHeroService {
+  constructor() {}
+
+  public getSuperHeroes(): SuperHero[] {
+    let hero = new SuperHero();
+    hero.id = 1;
+    hero.name = 'Spider-man';
+    hero.firstName = 'Peter';
+    hero.lastName = 'Parker';
+    hero.place = 'New York City';
+
+    return [hero];
+  }
+}
