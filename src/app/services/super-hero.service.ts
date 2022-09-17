536a3c96727f45e67e61e0ca5b3127de56b24042
Jose Victor Persset <jpersset@gmail.com>
2022-09-17 13:49:37 -0300

    Call the Web API from Angular Client

--- src/app/services/super-hero.service.ts
+++ src/app/services/super-hero.service.ts
@@ -1,20 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
 import { SuperHero } from '../models/super-hero';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SuperHeroService {
-  constructor() {}
+  private url = 'SuperHero';
 
-  public getSuperHeroes(): SuperHero[] {
-    let hero = new SuperHero();
-    hero.id = 1;
-    hero.name = 'Spider-man';
-    hero.firstName = 'Peter';
-    hero.lastName = 'Parker';
-    hero.place = 'New York City';
+  constructor(private http: HttpClient) {}
 
-    return [hero];
+  public getSuperHeroes(): Observable<SuperHero[]> {
+    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
   }
 }
